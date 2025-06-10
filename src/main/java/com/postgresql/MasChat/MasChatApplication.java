e4af8cc3335db302297caa1faf4a89fc44712a30
Kelvin Senior <sarfokelvinsenior@gmail.com>
2025-06-10 06:02:49 +0000

    Initial commit

---
+++ src/main/java/com/postgresql/MasChat/MasChatApplication.java
@@ -0,0 +1,11 @@
+package com.postgresql.MasChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasChatApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MasChatApplication.class, args);
+    }
+}
---
+++ src/main/java/com/postgresql/MasChat/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.postgresql.MasChat.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.postgresql.MasChat.model.User;
+import com.postgresql.MasChat.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+    @GetMapping("/{username}")
+    public User getUser(@PathVariable String username) {
+        return userService.findByUsername(username).orElse(null);
+    }
+}
