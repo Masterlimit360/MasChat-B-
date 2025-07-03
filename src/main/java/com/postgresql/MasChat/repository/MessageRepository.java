1bb9ad3ddfdd804a7ec4f356591c0c9a0fe8192b
Samuel Kofi Asante <170520620+Auel44@users.noreply.github.com>
2025-07-03 15:04:06 -0800

    Add files via upload

--- src/main/java/com/postgresql/MasChat/repository/MessageRepository.java
+++ src/main/java/com/postgresql/MasChat/repository/MessageRepository.java
@@ -1,6 +1,7 @@
 package com.postgresql.MasChat.repository;
 
 import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/postgresql/MasChat/repository/UserRepository.java
+++ src/main/java/com/postgresql/MasChat/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package com.postgresql.MasChat.repository;
 
-import com.postgresql.MasChat.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.postgresql.MasChat.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsername(String username);
